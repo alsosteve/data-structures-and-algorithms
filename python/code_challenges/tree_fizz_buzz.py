32844c8283c6c4821e9fe16c00f9b967061cc776
Steve Ngo <stevehngo@hotmail.com>
2022-05-12 09:47:38 -0400

    code almost done

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,26 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree
+from data_structures.queue import Queue
+
+def fizz_buzz_tree(tree):
+    q = Queue()
+    new_tree = KaryTree(tree.root)
+
+    q.enqueue(new_tree.root)
+    while not q.is_empty():
+        front = q.dequeue()
+        front.value = function(front.value)
+        for i in front.children:
+            q.enqueue(i)
+
+    return new_tree
 
 
-def fizz_buzz_tree():
-    pass
+def function(num):
+    if num % 15 == 0:
+        return "FizzBuzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
