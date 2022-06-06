e5a648cddd06ec38d93938d60fc20e7b99e94338
Steve Ngo <stevehngo@hotmail.com>
2022-06-06 04:42:56 -0400

    update tests and import demo code

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,29 +6,47 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
+def test_create_default():
+    ht = Hashtable()
+    actual = ht.size
+    expected = 1024
+    assert actual == expected
+
+
+def test_hash():
+    """
+    hash
+Arguments: key
+Returns: Index in the collection for that key
+    """
+    ht = Hashtable()
+    index = ht.hash("cat")
+    assert 0 <= index < ht.size
+
+
+def test_set_apple():
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+    fruit_index = ht.hash("fruit")
+    actual = ht.buckets[fruit_index]
+    expected = ("fruit", "apple")
+    assert actual.head.value == expected
+
+
 def test_get_apple():
-    hashtable = Hashtable()
-    hashtable.set("apple", "Used for apple sauce")
-    actual = hashtable.get("apple")
-    expected = "Used for apple sauce"
+    ht = Hashtable()
+    ht.set("fruit", "apple")
+    actual = ht.get("fruit")
+    expected = "apple"
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
-def test_internals():
-    hashtable = Hashtable(1024)
-    hashtable.set("ahmad", 30)
-    hashtable.set("silent", True)
-    hashtable.set("listen", "to me")
+def test_collisions():
+    ht = Hashtable()
+    ht.set("cat", "Josie")
+    ht.set("act", "A Contemporary Theater")
+    ht.set("tac", "Taco Tuesday")
 
-    actual = []
-
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
-    for item in hashtable._buckets:
-        if item:
-            actual.append(item.display())
-
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
-
-    assert actual == expected
+    assert ht.get("cat") == "Josie"
+    assert ht.get("act") == "A Contemporary Theater"
+    assert ht.get("tac") == "Taco Tuesday"
