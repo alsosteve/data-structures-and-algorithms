638bc872cc1ebf2ff10b762787751c805536cbff
Steve Ngo <stevehngo@hotmail.com>
2022-06-07 00:08:20 -0400

    tests passed

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,14 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+
+def left_join(a, b):
+    # joins two hashmaps into a single data structure
+    output = []
+
+    for key in a:
+        join_left = [key, a.get(key), b.get(key)or "NONE"]
+        output.append(join_left)
+
+    # return output
+    return output
